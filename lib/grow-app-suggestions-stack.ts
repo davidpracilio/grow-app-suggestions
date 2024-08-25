5c01c455df4e9b172fc6f4c69d984ba132c23b0b
davidpracilio <dave.pracilio@outlook.com>
2024-08-25 21:42:30 +0800

    Create lambda and api gateway resources

---
+++ lib/grow-app-suggestions-stack.ts
@@ -0,0 +1,33 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+
+export class GrowAppSuggestionsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Define the Lambda function resource
+    const growAppSuggestionsFunction = new lambda.Function(this, 'GrowAppSuggestionsFunction', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'grow-app-suggestions.handler', // Points to the file in the lambda directory
+    });
+
+    // Define the API Gateway resource
+    const api = new apigateway.LambdaRestApi(this, 'GrowAppSuggestionsApi', {
+      handler: growAppSuggestionsFunction,
+      proxy: false,
+    });
+
+    // Define the '/learning-suggestions resource with a GET method
+    const learningSuggestionsResource = api.root.addResource('learning-suggestions');
+    learningSuggestionsResource.addMethod('GET');
+
+    // Output as a separate stack output
+    new cdk.CfnOutput(this, 'growAppSuggestionsApiUrl', {
+      value: api.url,
+      description: 'The URL of the API Gateway',
+    });
+  }
+}
---
+++ test/grow-app-suggestions.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GrowAppSuggestions from '../lib/grow-app-suggestions-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/grow-app-suggestions-stack.ts
+// test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GrowAppSuggestions.GrowAppSuggestionsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+// });
