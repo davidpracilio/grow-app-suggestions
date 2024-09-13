a84d66b2f769b14f8a4658c9bdc32d415bc89c63
davidpracilio <dave.pracilio@outlook.com>
2024-09-13 23:50:30 +0800

    Add access control methods to lambda handler

--- lambda/grow-app-suggestions.js
+++ lambda/grow-app-suggestions.js
@@ -78,6 +78,11 @@ exports.handler = async (event) => {
 
     return {
       statusCode: 200,
+      headers: {
+          'Access-Control-Allow-Headers': 'Content-Type',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'OPTIONS,GET'
+      },
       body: JSON.stringify(learningSuggestions),
     };
   } catch (error) {
